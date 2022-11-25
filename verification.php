42e4c676fc9d207e092a18f355679494fcf27a8d
Defofloriane <88293985+Defofloriane@users.noreply.github.com>
2022-11-25 15:27:34 +0100

    finish tp

--- verification.php
+++ verification.php
@@ -6,7 +6,7 @@
 * E-mail : florianekemgne@gmail.com
 ****************************************************************
 * Date de création : 20-11-2022 (21 Novembre 2022)
-* Dernière modification : 22-11-2022
+* Dernière modification : 25-11-2022
 ****************************************************************
 * Historique des modifications
 * 21-11-2022: Le script verification.php se connecter a la base de donnee et afficher le tableau journal
@@ -26,41 +26,49 @@ function getIp(){
   }
 if (isset ($_POST['validate']) ) {
     $mat = $_POST['mat'];
-    $password = $_POST['pwd'];
+    $password = md5($_POST['pwd']) ;
     $satut = "Echec - Matricule incorrect";
     $adresse = getIp();
     $date_jr = date('d/m/Y');
         $sql = "SELECT noms,prenoms,password_sect,matricule FROM etudiants WHERE matricule='".$mat."' ";
+        $sql2 = "SELECT password_sect FROM etudiants WHERE password_sect='".$password."' ";
         //$sql = "SELECT noms FROM etudiants WHERE matricule='".$mat."' AND password_sect='".$password."'";
         $req =  $connection->query($sql);
         $result = $req->fetch(PDO::FETCH_ASSOC);
+        $req1 =  $connection->query($sql);
+        $result1 = $req1->fetch(PDO::FETCH_ASSOC);
         try {
-          if (is_array($result) || is_object($result))
+          if (is_array($result) || is_object($result) || is_array($result1) || is_object($result1))
             {
-                foreach ($result as $keys => $value1)
+                foreach ($result as $keys => $value1 )
                 {
-                  while($mat == $value1){
-                    $statut = "Succes ";
-                    echo "$value1</br>";
-                  
-                  
-                  
-                    $sql_insert = 'INSERT INTO journal(adresse,date_journal,matricule,statut) VALUES(:adresse, :date_journal, :matricule ,:statut)';
-                        $statement = $connection->prepare($sql_insert);
-                        if ($statement->execute([':adresse' => $adresse,':date_journal' => $date_jr,':matricule' => $mat,':statut' => $statut])) {
-                        echo"insert data dans la table journal";
-                        }   
-                        session_start();
-                        $_SESSION["mat"]= $mat;
-                        $_SESSION["message"] = "Bienvenue Mr/Mme ".$result["noms"]." ".$result["prenoms"]; 
-                        header("location: accueil.php");
-                       $value1 = "hello";
-                  
-                      }
+                  foreach($result1 as $key => $value2) {
+                    while($mat == $value1 && $password == $value2 ){
+                      $statut = "Succes ";
+                      echo "$value1</br>";
+                      echo "$value2</br>";
+                    
+                    
+                    
+                      $sql_insert = 'INSERT INTO journal(adresse,date_journal,matricule,statut) VALUES(:adresse, :date_journal, :matricule ,:statut)';
+                          $statement = $connection->prepare($sql_insert);
+                          if ($statement->execute([':adresse' => $adresse,':date_journal' => $date_jr,':matricule' => $mat,':statut' => $statut])) {
+                          echo"insert data dans la table journal";
+                          }   
+                          session_start();
+                          $_SESSION["mat"]= $mat;
+                          $_SESSION["message"] = "Bienvenue Mr/Mme ".$result["noms"]." ".$result["prenoms"]; 
+                          header("location: accueil.php");
+                         $value1 = "hello";
+                    
+                        }
+                  }
+                 
                  
 
                 }
-
+    echo "Mot de passe incorrect,Retour a la connection!.</br>";
+              
             }else {
               echo"none result";
               $sql_insert = 'INSERT INTO journal(adresse,date_journal,matricule,statut) VALUES(:adresse, :date_journal, :matricule ,:statut)';
